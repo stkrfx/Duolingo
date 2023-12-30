c8f184e8628c53aa2fdec2605371e48365b1b37e
Satyam <stkrfx@gmail.com>
2023-12-31 03:21:27 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  const [user, setUser] = React.useState();
+  const [loaded, setLoaded] = React.useState();
+
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Duolingo.Provider value={{user, setUser, theme}}>
+        <StatusBar
+          backgroundColor={theme.backgrounds.primary}
+          barStyle={theme.colors.barStyle}
+        />
+        {loaded ? (
+          <NavigationContainer>
+            <Stack.Navigator
+              screenOptions={{
+                headerShown: false,
+                freezeOnBlur: true,
+              }}>
+              <Stack.Screen name="onboarding" component={Onboarding} />
+              <Stack.Screen name="chats" component={Chats} />
+              <Stack.Screen name="new" component={New} />
+              <Stack.Screen name="chat" component={Chat} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        ) : (
+          <Splash />
+        )}
+      </Duolingo.Provider>
+    </GestureHandlerRootView>
+  );
+};
 
 AppRegistry.registerComponent(appName, () => App);
